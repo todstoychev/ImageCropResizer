fa01654129460a7bdf8552664e271f471703d4c8
Todor Todorov <todstoychev@gmail.com>
2014-10-27 17:31:07 +0200

    Tets

--- src/config/contexts.php
+++ src/config/contexts.php
@@ -4,15 +4,18 @@ return [
     'default' => [
         'small' => [
             'width' => 100,
-            'height' => 100
+            'height' => 100,
+            'action' => 'crop-resize'
         ],
         'medium' => [
             'width' => 200,
-            'height' => 200
+            'height' => 200,
+            'action' => 'crop-resize'
         ],
         'large' => [
             'width' => 300,
-            'height' => 300
+            'height' => 300,
+            'action' => 'crop-resize'
         ]
     ]
 ];
